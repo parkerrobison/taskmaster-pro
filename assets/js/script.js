0d04bdcbfa7704efa4447133093ba9354760437b
probison <parker.robison@hotmail.com>
2020-05-26 23:03:29 -0600

    added calendar picker to modal and to change date event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,12 +109,21 @@ $(".list-group").on("click", "span", function (){
   //swap out elements
   $(this).replaceWith(dateInput);
 
+  //enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      //when calendar is closed, force a "change" on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
   //automatically focus on new element
   dateInput.trigger("focus");
 });
 
 //value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function(){
+$(".list-group").on("change", "input[type='text']", function(){
   //get current text
   var date = $(this)
   .val()
@@ -228,6 +237,10 @@ $("#task-form-modal .btn-primary").click(function() {
 
     saveTasks();
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // remove all tasks
